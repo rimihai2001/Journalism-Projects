f04ceb140e1a823f9c2c9e05f8c644bc69524633
Radu-Ioan Mihai <rimihai2001@gmail.com>
2024-04-27 20:24:08 +0300

    Add files via upload

---
+++ Libertatea/Harta Sanselor/Interactive/script.js
@@ -0,0 +1,335 @@
+var jsonData = {
+    "ALBA": {
+        "salariu_med_net": 3923,
+        "speranta_viata": 76.76,
+        "rata_somaj": 3.55,
+        "promovare_en": 77.9,
+        "promovare_bac": 81.0
+    },
+    "ARAD": {
+        "salariu_med_net": 3809,
+        "speranta_viata": 75.38,
+        "rata_somaj": 1.18,
+        "promovare_en": 69.4,
+        "promovare_bac": 71.0
+    },
+    "ARGEȘ": {
+        "salariu_med_net": 4013,
+        "speranta_viata": 76.16,
+        "rata_somaj": 3.55,
+        "promovare_en": 77.5,
+        "promovare_bac": 78.4
+    },
+    "BACĂU": {
+        "salariu_med_net": 3933,
+        "speranta_viata": 74.6,
+        "rata_somaj": 2.4,
+        "promovare_en": 75.8,
+        "promovare_bac": 81.9
+    },
+    "BIHOR": {
+        "salariu_med_net": 3555,
+        "speranta_viata": 75.53,
+        "rata_somaj": 1.31,
+        "promovare_en": 75.7,
+        "promovare_bac": 76.5
+    },
+    "BISTRIȚA-NĂSĂUD": {
+        "salariu_med_net": 3542,
+        "speranta_viata": 76.4,
+        "rata_somaj": 3.13,
+        "promovare_en": 78.5,
+        "promovare_bac": 77.9
+    },
+    "BOTOȘANI": {
+        "salariu_med_net": 3564,
+        "speranta_viata": 74.97,
+        "rata_somaj": 2.97,
+        "promovare_en": 66.2,
+        "promovare_bac": 71.2
+    },
+    "BRĂILA": {
+        "salariu_med_net": 3542,
+        "speranta_viata": 74.84,
+        "rata_somaj": 3.11,
+        "promovare_en": 82.8,
+        "promovare_bac": 84.7
+    },
+    "BRAȘOV": {
+        "salariu_med_net": 4139,
+        "speranta_viata": 77.37,
+        "rata_somaj": 2.44,
+        "promovare_en": 78.3,
+        "promovare_bac": 80.9
+    },
+    "BUZĂU": {
+        "salariu_med_net": 3632,
+        "speranta_viata": 75.33,
+        "rata_somaj": 5.24,
+        "promovare_en": 74.1,
+        "promovare_bac": 77.1
+    },
+    "CĂLĂRAȘI": {
+        "salariu_med_net": 3613,
+        "speranta_viata": 74.21,
+        "rata_somaj": 5.1,
+        "promovare_en": 63.2,
+        "promovare_bac": 60.6
+    },
+    "CARAȘ-SEVERIN": {
+        "salariu_med_net": 3410,
+        "speranta_viata": 75.63,
+        "rata_somaj": 2.43,
+        "promovare_en": 70.8,
+        "promovare_bac": 67.7
+    },
+    "CLUJ": {
+        "salariu_med_net": 5525,
+        "speranta_viata": 77.47,
+        "rata_somaj": 1.16,
+        "promovare_en": 87.4,
+        "promovare_bac": 84.3
+    },
+    "CONSTANȚA": {
+        "salariu_med_net": 3892,
+        "speranta_viata": 75.73,
+        "rata_somaj": 1.96,
+        "promovare_en": 74.2,
+        "promovare_bac": 76.5
+    },
+    "COVASNA": {
+        "salariu_med_net": 3657,
+        "speranta_viata": 75.65,
+        "rata_somaj": 5.33,
+        "promovare_en": 71.3,
+        "promovare_bac": 67.9
+    },
+    "DÂMBOVIȚA": {
+        "salariu_med_net": 3665,
+        "speranta_viata": 75.8,
+        "rata_somaj": 2.96,
+        "promovare_en": 69.2,
+        "promovare_bac": 68.7
+    },
+    "DOLJ": {
+        "salariu_med_net": 3925,
+        "speranta_viata": 75.36,
+        "rata_somaj": 7.1,
+        "promovare_en": 73.2,
+        "promovare_bac": 70.8
+    },
+    "GALAȚI": {
+        "salariu_med_net": 3913,
+        "speranta_viata": 75.37,
+        "rata_somaj": 4.9,
+        "promovare_en": 80.1,
+        "promovare_bac": 81.6
+    },
+    "GIURGIU": {
+        "salariu_med_net": 3824,
+        "speranta_viata": 73.96,
+        "rata_somaj": 3.36,
+        "promovare_en": 70.5,
+        "promovare_bac": 61.3
+    },
+    "GORJ": {
+        "salariu_med_net": 4134,
+        "speranta_viata": 76.33,
+        "rata_somaj": 3.02,
+        "promovare_en": 75.4,
+        "promovare_bac": 64.1
+    },
+    "HARGHITA": {
+        "salariu_med_net": 3548,
+        "speranta_viata": 76.14,
+        "rata_somaj": 3.85,
+        "promovare_en": 72.6,
+        "promovare_bac": 62.0
+    },
+    "HUNEDOARA": {
+        "salariu_med_net": 3417,
+        "speranta_viata": 75.27,
+        "rata_somaj": 4.38,
+        "promovare_en": 74.5,
+        "promovare_bac": 76.0
+    },
+    "IALOMIȚA": {
+        "salariu_med_net": 3654,
+        "speranta_viata": 74.57,
+        "rata_somaj": 4.69,
+        "promovare_en": 68.2,
+        "promovare_bac": 69.0
+    },
+    "IAȘI": {
+        "salariu_med_net": 4363,
+        "speranta_viata": 76.55,
+        "rata_somaj": 3.08,
+        "promovare_en": 77.9,
+        "promovare_bac": 82.2
+    },
+    "ILFOV": {
+        "salariu_med_net": 4582,
+        "speranta_viata": 76.27,
+        "rata_somaj": 0.46,
+        "promovare_en": 76.1,
+        "promovare_bac": 51.9
+    },
+    "MARAMUREȘ": {
+        "salariu_med_net": 3561,
+        "speranta_viata": 75.62,
+        "rata_somaj": 2.34,
+        "promovare_en": 76.2,
+        "promovare_bac": 70.3
+    },
+    "MEHEDINȚI": {
+        "salariu_med_net": 3664,
+        "speranta_viata": 75.64,
+        "rata_somaj": 6.69,
+        "promovare_en": 69.5,
+        "promovare_bac": 62.1
+    },
+    "MUNICIPIUL BUCUREȘTI": {
+        "salariu_med_net": 5842,
+        "speranta_viata": 78.4,
+        "rata_somaj": 0.92,
+        "promovare_en": 88.9,
+        "promovare_bac": 78.8
+    },
+    "MUREȘ": {
+        "salariu_med_net": 4168,
+        "speranta_viata": 75.82,
+        "rata_somaj": 2.93,
+        "promovare_en": 71.5,
+        "promovare_bac": 68.4
+    },
+    "NEAMȚ": {
+        "salariu_med_net": 3526,
+        "speranta_viata": 75.73,
+        "rata_somaj": 4.37,
+        "promovare_en": 71.3,
+        "promovare_bac": 78.1
+    },
+    "OLT": {
+        "salariu_med_net": 3998,
+        "speranta_viata": 74.73,
+        "rata_somaj": 5.19,
+        "promovare_en": 74.7,
+        "promovare_bac": 69.4
+    },
+    "PRAHOVA": {
+        "salariu_med_net": 4128,
+        "speranta_viata": 75.88,
+        "rata_somaj": 2.51,
+        "promovare_en": 82.3,
+        "promovare_bac": 78.2
+    },
+    "SĂLAJ": {
+        "salariu_med_net": 3780,
+        "speranta_viata": 75.27,
+        "rata_somaj": 4.9,
+        "promovare_en": 71.9,
+        "promovare_bac": 69.0
+    },
+    "SATU MARE": {
+        "salariu_med_net": 3604,
+        "speranta_viata": 74.24,
+        "rata_somaj": 3.54,
+        "promovare_en": 73.2,
+        "promovare_bac": 75.2
+    },
+    "SIBIU": {
+        "salariu_med_net": 4694,
+        "speranta_viata": 76.9,
+        "rata_somaj": 2.7,
+        "promovare_en": 76.2,
+        "promovare_bac": 79.5
+    },
+    "SUCEAVA": {
+        "salariu_med_net": 3445,
+        "speranta_viata": 76.31,
+        "rata_somaj": 5.19,
+        "promovare_en": 78.3,
+        "promovare_bac": 77.5
+    },
+    "TELEORMAN": {
+        "salariu_med_net": 3194,
+        "speranta_viata": 75.19,
+        "rata_somaj": 7.76,
+        "promovare_en": 62.1,
+        "promovare_bac": 61.8
+    },
+    "TIMIȘ": {
+        "salariu_med_net": 4954,
+        "speranta_viata": 76.85,
+        "rata_somaj": 0.87,
+        "promovare_en": 76.1,
+        "promovare_bac": 75.6
+    },
+    "TULCEA": {
+        "salariu_med_net": 3438,
+        "speranta_viata": 73.24,
+        "rata_somaj": 3.81,
+        "promovare_en": 71.5,
+        "promovare_bac": 69.0
+    },
+    "VÂLCEA": {
+        "salariu_med_net": 3421,
+        "speranta_viata": 81.2,
+        "rata_somaj": 3.33,
+        "promovare_en": 77.6,
+        "promovare_bac": 75.7
+    },
+    "VASLUI": {
+        "salariu_med_net": 3588,
+        "speranta_viata": 75.06,
+        "rata_somaj": 7.49,
+        "promovare_en": 63.7,
+        "promovare_bac": 70.9
+    },
+    "VRANCEA": {
+        "salariu_med_net": 3275,
+        "speranta_viata": 75.8,
+        "rata_somaj": 3.81,
+        "promovare_en": 71.7,
+        "promovare_bac": 75.7
+    }
+};
+
+var selectElement = document.getElementById("jsonSelect");
+var countyValuesElement = document.getElementById("countyValues");
+
+for (var key in jsonData) {
+  if (jsonData.hasOwnProperty(key)) {
+    var option = document.createElement("option");
+    option.value = key;
+    option.text = key; 
+    selectElement.appendChild(option);
+  }
+}
+
+selectElement.addEventListener("change", function() {
+  updateValues();
+});
+
+function updateValues() {
+  var selectedCounty = selectElement.value;
+  
+  
+  var selectedCountyData = jsonData[selectedCounty];
+
+  countyValuesElement.innerHTML = `
+    <h3>Dacă ești în ${selectedCounty}...</h3>
+    <p>Te poți aștepta la un
+      salariu mediu net de aproximativ
+      <strong>${ selectedCountyData.salariu_med_net } RON</strong> potrivit datelor INS pe septembrie 2023. Rata șomajului este de <strong>${ selectedCountyData.rata_somaj}%</strong>.</p>
+    <p>Speranța ta de viață, de aproximativ
+     <strong> ${ selectedCountyData.speranta_viata } ani</strong>, reflectă grija acordată atât
+      domeniului sănătății, cât și calității vieții tale.</p>
+    <p>Cum te-ai descurcat la examene? În anul 2023, <strong>${ selectedCounty }</strong> a
+      înregistrat o rată de promovare a Bacalaureatului de
+      <strong>${ selectedCountyData.promovare_bac }%</strong>, în timp ce la Evaluarea
+      Națională, <strong>${ selectedCountyData.promovare_en}%</strong> dintre elevii înscriși
+      au obținut cel puțin nota 5.</p>
+    <p>Cum ți se par aceste valori? Alege și alte județe pentru comparație.</p>
+  `;
+}
